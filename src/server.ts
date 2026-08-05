d0f1035905e2870fe9b037c90cfabb261d99be44
Divyansh Gupta <guptadivyansh19032007@gmail.com>
2026-08-05 17:50:57 +0000

    Setup server

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+
+process.on("uncaughtException", (err: Error) => {
+    console.log("UNCAUGHT EXCEPTION: Shutting down...");
+    console.log(`${err.name}: ${err.message}`);
+    console.log(err.stack);
+    process.exit(1);
+});
+
+dotenv.config({ path: "../.env" });
+
+import app from "./app.js";
+
+const port = 3000;
+const server = app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err: Error) => {
+    console.log("UNHANDLED REJECTION: Shutting down...");
+    console.log(`${err.name}: ${err.message}`);
+    server.close(() => process.exit(1));
+});
