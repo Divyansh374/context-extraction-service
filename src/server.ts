5bada03889796d4b78a163af41c4ce1dc0fcc60a
Divyansh Gupta <guptadivyansh19032007@gmail.com>
2026-08-05 18:03:30 +0000

    Refine server configuration

--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,10 @@ process.on("uncaughtException", (err: Error) => {
 dotenv.config({ path: "../.env" });
 
 import app from "./app.js";
+import { type Server } from "node:http";
 
-const port = 3000;
-const server = app.listen(port, () => {
+const port: string | number = process.env.PORT || 3000;
+const server: Server = app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
 
