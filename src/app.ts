673d763045b1589c98a5edc70c13894d8429b02a
Divyansh Gupta <guptadivyansh19032007@gmail.com>
2026-08-05 18:15:09 +0000

    Implement request routing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { type Express } from "express";
+import extractRouter from "./routes/extract.routes.js";
 
 const app: Express = express();
 
 app.use(express.json());
+
+app.use("/api/v1/extract", extractRouter);
 
 export default app;
