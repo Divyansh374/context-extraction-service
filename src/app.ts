1d917f483f4041d162e6ad31478e9e55138b3f20
Divyansh Gupta <guptadivyansh19032007@gmail.com>
2026-08-06 05:37:29 +0000

    setup global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { type Express } from "express";
 import extractRouter from "./routes/extract.routes.js";
+import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 
 const app: Express = express();
 
 app.use(express.json());
 
 app.use("/api/v1/extract", extractRouter);
+
+app.use(globalErrorHandler);
 
 export default app;
---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,20 @@
+import { type Response, type Request } from "express";
+import AppError from "../utils/AppError.js";
+
+const sendErrorDev = (err: AppError, res: Response) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack,
+    });
+}
+
+export default (err: AppError, req: Request, res: Response) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+
+    if (process.env.NODE_ENV === "development") {
+        sendErrorDev(err, res);
+    }
+}
