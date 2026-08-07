5fb2592eba780e083bc872ababaad29d4729302e
Divyansh Gupta <guptadivyansh19032007@gmail.com>
2026-08-07 12:27:53 +0000

    Refactor scraping reddit posts into two seperate functions

--- src/scrapers/reddit.scraper.ts
+++ src/scrapers/reddit.scraper.ts
@@ -1,44 +1,54 @@
 import { NextFunction } from "express";
 import { MAX_SEARCHES } from "../constants/pipeline.constants.js";
 import { Keyword } from "../types/keyword.js";
-import AppError from "../utils/AppError.js";
 import { ScrapedPost } from "../types/scrapedPost.js";
+import AppError from "../utils/AppError.js";
 
-export const searchReddit = async (
+const getPosts = async (keyword: Keyword, next: NextFunction) => {
+    let response, data;
+
+    try {
+        const query = keyword.content.split(" ");
+        response = await fetch(
+            `https://api.pullpush.io/reddit/search/submission?q=${query.join("+")}&size=10&sort=desc&sort_type=score`,
+        );
+        data = await response.json();
+    } catch {
+        return next(new AppError(502, "The requested server is not working"));
+    }
+
+    const result = new Map<string, ScrapedPost>();
+
+    data.data.forEach((item) => {
+        result.set(item.id, {
+            id: item.id,
+            platform: "reddit",
+            subreddit: item.subreddit,
+            title: item.title,
+            content: item.selftext,
+            author: item.author,
+            url: `https://reddit.com/${item.permalink}`,
+            engagement: item.score,
+            createdAt: new Date(),
+        });
+    });
+
+    return [...result.values()];
+};
+
+export const compilePosts = async (
     req: Request,
     res: Response,
     next: NextFunction,
     keywords: Keyword[],
 ) => {
     const search_cap = MAX_SEARCHES(keywords);
+    const posts: ScrapedPost[] = [];
 
     for (let i: number = 0; i < search_cap; i++) {
-        let response, data;
-
-        try {
-            const query = keywords[i].content.split(" ");
-            response = await fetch(
-                `https://api.pullpush.io/reddit/search/submission?q=${query.join("+")}&size=10&sort=desc&sort_type=score`,
-            );
-            data = await response.json();
-        } catch {
-            return next(new AppError(502, "The requested server is not working"));
-        }
-
-        const result = new Set<ScrapedPost>();
-
-        data.data.forEach((item) => {
-            result.add({
-                id: item.id,
-                platform: "reddit",
-                subreddit: item.subreddit,
-                title: item.title,
-                content: item.selftext,
-                author: item.author,
-                url: `https://reddit.com/${item.permalink}`,
-                engagement: item.score,
-                createdAt: new Date(),
-            });
-        });
+        const posts_set = await getPosts(keywords[i], next);
+        if (posts_set) posts.push(...posts_set);
     }
+
+    return posts;
 };
