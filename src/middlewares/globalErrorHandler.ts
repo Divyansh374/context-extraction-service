02d6e8719bc43b7d8caab1dd2c684ea96412b021
Divyansh Gupta <guptadivyansh19032007@gmail.com>
2026-08-08 13:04:53 +0000

    Implement error handling for production environment

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -10,11 +10,27 @@ const sendErrorDev = (err: AppError, res: Response) => {
     });
 };
 
+const sendErrorProd = (err: AppError, res: Response) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    } else {
+        res.status(500).json({
+            status: "error",
+            message: "something went wrong",
+        });
+    }
+};
+
 export default (err: AppError, req: Request, res: Response) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
 
     if (process.env.NODE_ENV === "development") {
         sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === "production") {
+        sendErrorProd(err, res);
     }
 };
